6b9a7606f7fc1ef57175bcf68efb77e160f9abb2
Mahbubur Rahman <rifat0153@gmail.com>
2021-03-25 12:05:28 +0600

    Category Item added to cat screen

--- lib/screens/categories_screens.dart
+++ lib/screens/categories_screens.dart
@@ -1,18 +1,24 @@
 import 'package:flutter/material.dart';
 
+import '../data/dummy_data.dart';
+import '../widget/category_item.dart';
+
 class CategoriesScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GridView(
+      children: DUMMY_CATEGORIES.map((cat) {
+        CategoryItem(
+          cat.title,
+          cat.color,
+        );
+      }).toList(),
       gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
         maxCrossAxisExtent: 200,
         childAspectRatio: 3 / 2,
         crossAxisSpacing: 20,
         mainAxisSpacing: 20,
       ),
-      children: <Widget>[
-        
-      ],
     );
   }
 }
