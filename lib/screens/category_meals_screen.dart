32315480ec1c73618d8b1ed7fd00dc7bf0708ef7
Mahbubur Rahman <rifat0153@gmail.com>
2021-03-25 14:28:09 +0600

    named Route added with data passing

--- lib/screens/category_meals_screen.dart
+++ lib/screens/category_meals_screen.dart
@@ -1,14 +1,27 @@
 import 'package:flutter/material.dart';
 
 class CategoryMealsScreen extends StatelessWidget {
+  // final String categoryId;
+  // final String categoryTitle;
+
+  // CategoryMealsScreen(this.categoryId, this.categoryTitle);
+
   @override
   Widget build(BuildContext context) {
+    final routeArgs =
+        ModalRoute.of(context).settings.arguments as Map<String, String>;
+
+    final categoryTitle = routeArgs['title'];
+    final categoryId = routeArgs['id'];
+
     return Scaffold(
       appBar: AppBar(
-        title: Text('The Recipe'),
+        title: Text(categoryTitle),
       ),
       body: Center(
-        child: Text('Meals'),
+        child: Text(
+          'The Recipies for the Category',
+        ),
       ),
     );
   }
