76c485c9a64f09692b4d718e1d9e581a26cbaa7d
Mahbubur Rahman <rifat0153@gmail.com>
2021-03-25 12:56:27 +0600

    font added

--- lib/main.dart
+++ lib/main.dart
@@ -12,28 +12,24 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.pink,
+        accentColor: Colors.amber,
+        canvasColor: Color.fromRGBO(255, 254, 229, 1),
+        fontFamily: 'Raleway',
+        textTheme: ThemeData.light().textTheme.copyWith(
+              bodyText1: TextStyle(
+                color: Color.fromRGBO(20, 51, 51, 1),
+              ),
+              bodyText2: TextStyle(
+                color: Color.fromRGBO(20, 51, 51, 1),
+              ),
+              headline6: TextStyle(
+                fontSize: 24,
+                fontFamily: 'Roboto',
+              ) 
+            ),
       ),
       home: CategoriesScreen(),
     );
   }
 }
-
-// class MyHomePage extends StatefulWidget {
-//   @override
-//   _MyHomePageState createState() => _MyHomePageState();
-// }
-
-// class _MyHomePageState extends State<MyHomePage> {
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       appBar: AppBar(
-//         title: Text('DeliMeals'),
-//       ),
-//       body: Center(
-//         child: Text('Nav'),
-//       ),
-//     );
-//   }
-// }
--- lib/screens/categories_screens.dart
+++ lib/screens/categories_screens.dart
@@ -11,6 +11,7 @@ class CategoriesScreen extends StatelessWidget {
         title: const Text('Categories'),
       ),
       body: GridView(
+        padding: const EdgeInsets.all(25),
         children: DUMMY_CATEGORIES.map((cat) {
           return CategoryItem(
             cat.title,
